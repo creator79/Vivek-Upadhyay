12eb0a162a83ddc034da2af016ca955b348cdc7a
mahi-19 <upadhyayvivek.19@gmail.com>
2023-03-21 12:56:57 +0530

    added Styles

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
 import { motion } from "framer-motion";
-
-import { styles } from "../styles";
-import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
@@ -23,17 +19,17 @@ function Resume() {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+      Welcome to my portfolio website! If you're here, it's likely that you're interested in learning more about me and my skills. If you want to take a closer look at my professional experience, click on the <strong>"Resume"</strong> button below. Get ready to be impressed with my expertise in various fields such as project management, content creation, and social media strategy. My resume highlights my achievements, education, and work history, demonstrating my dedication to excellence and my commitment to delivering high-quality work. Don't miss out on the chance to see my impressive resume and learn more about what I can bring to your team. Click that button now!
       
       </motion.p>
+     
 
   <button 
   onClick={handleResumeClick} 
-  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-aling-center mx-[70vh] mt-[3vh] mb-[-80vh]"
+  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded text-center mx-auto md:mx-[50vh] mt-6 xl:mt-[3vh] mb-[-80vh] md:mb-0 xl:mb-0 hover:shadow-blue" 
+  
+
+
 >
   Resume
 </button>
